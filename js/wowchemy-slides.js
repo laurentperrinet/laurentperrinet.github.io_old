7dec10b54b2d43bf86d8b3521f6dd5ce3a109413
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-07-19 12:57:06 +0200

    rebuilding site Mon Jul 19 12:57:06 CEST 2021

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {theme: "white"};
+  var slides = {theme: "black"};
 
   // ns-hugo:/var/folders/3p/m0g52j9j69z3gj8ktpgg1dm00000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20210701193729-94ce6a21f268/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
