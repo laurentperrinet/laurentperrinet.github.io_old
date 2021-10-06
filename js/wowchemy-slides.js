3e5e95adc62846294012e03eb1709c6f07f2ae9d
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-10-06 13:30:12 +0200

    rebuilding site Wed Oct  6 13:30:11 CEST 2021

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,8 +1,8 @@
 (() => {
   // ns-params:@params
-  var slides = { theme: "black" };
+  var slides = { "font-size": "10pt", "font-style": "bold", theme: "white" };
 
-  // ns-hugo:/var/folders/3p/m0g52j9j69z3gj8ktpgg1dm00000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20210912234819-c9831b5c1e21/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/3p/m0g52j9j69z3gj8ktpgg1dm00000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20211004191917-b02a90a434c6/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
