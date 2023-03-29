30e9967028d1c4d3cfc5838079428a84b447aa64
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2023-03-29 12:28:11 +0200

    rebuilding site Wed Mar 29 12:28:11 CEST 2023

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = { theme: "black" };
+  var slides = { backgroundtransition: "fade", theme: "black", transition: "fade" };
 
   // <stdin>
   var enabledPlugins = [RevealMarkdown, RevealHighlight, RevealSearch, RevealNotes, RevealMath.MathJax3, RevealZoom];
